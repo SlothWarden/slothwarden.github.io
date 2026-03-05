983494a03e490b540482b1388c9655fede2532b8
SlothWarden <slothwarden@gmail.com>
2026-03-05 15:07:46 -0700

    Make Jar Jar Binks Hash An Option

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+function bluff() {
+document.getElementById("title").innerHTML = "Ya-hoo everybody! me'm slothwarden, and disa   ownen lil' page!";
+
+}
 <script>
 if (window.location.pathname.endsWith('.html')) {
     window.location.href = window.location.pathname.replace('.html', '');
 }
 </script>
+window.addEventListener("hashchange", () => {
+if (window. location.hash === "#jarjarbinks") {
+bluff();
+();
+}
+});
